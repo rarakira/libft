83ab30f92c8414a48ebf5f238089e4516974f6f0
Linda Baela <lbaela@at-h1.msk.21-school.ru>
2021-04-19 13:29:44 +0300

    Remove counters in memset & bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 12:04:07 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/19 13:16:35 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/04/19 13:27:59 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned int	i;
 	unsigned char	*str;
 
-	i = 0;
 	str = (unsigned char *)s;
-	while (i < n)
-	{
-		str[i] = '\0';
-		i++;
-	}
+	while (n > 0)
+		str[--n] = '\0';
 	return ;
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 11:34:02 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/19 13:16:46 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/04/19 13:23:24 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned int	i;
 	unsigned char	ch;
 	unsigned char	*str;
 
-	i = 0;
 	ch = (unsigned char)c;
 	str = (unsigned char *)b;
-	while (i < len)
-	{
-		str[i] = ch;
-		i++;
-	}
+	while (len > 0)
+		str[--len] = ch;
 	return (b);
 }
