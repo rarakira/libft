0a381f26317e2cd9881c8024df14e29239759761
Kira <rarakira@gmail.com>
2021-04-21 16:27:11 +0300

    Update strlcat, add tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 17:43:01 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/21 09:32:36 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/04/21 16:21:32 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    return (0);
+    size_t  srclen;
+    size_t  dstlen;
+    size_t  catlen;
+
+    srclen = ft_strlen(src);
+    dstlen = ft_strlen(dst);
+    catlen = dstsize - dstlen;
+    if (catlen > 1)
+    {
+        ft_memcpy(dst + dstlen, src, catlen - 1);
+        dst[srclen + dstlen] = '\0';
+    }
+    return (srclen + dstlen);
 }
