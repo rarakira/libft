2bdc60ac4733762215dd69d213ca2ccfc4ec89b4
Kira <rarakira@gmail.com>
2021-04-21 22:31:56 +0300

    Add strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,21 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/21 19:06:04 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/21 22:14:57 by lbaela           ###   ########.fr       */
+/*   Created: 2021/04/21 22:14:20 by lbaela            #+#    #+#             */
+/*   Updated: 2021/04/21 22:24:19 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	size_t	srcsize;
+	size_t			srcsize;
+	size_t			i;
+	unsigned char	ch;
+	char			*ptr;
 
+	ch = (unsigned char)c;
 	srcsize = ft_strlen(s);
-	return ((char *)ft_memchr(s, c, srcsize));
+	i = 0;
+	ptr = NULL;
+	while (i <= srcsize)
+	{
+		if (s[i] == ch)
+			ptr = (char *)s + i;
+		i++;
+	}
+	return (ptr);
 }
