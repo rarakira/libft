65d6562aebc3cb03e93d379089ad650f8e3bc837
Kira <rarakira@gmail.com>
2021-04-21 20:51:41 +0300

    Norm updates. Add strchr

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,25 +6,25 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 17:43:01 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/21 16:21:32 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/04/21 20:50:04 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t  srclen;
-    size_t  dstlen;
-    size_t  catlen;
+	size_t	srclen;
+	size_t	dstlen;
+	size_t	catlen;
 
-    srclen = ft_strlen(src);
-    dstlen = ft_strlen(dst);
-    catlen = dstsize - dstlen;
-    if (catlen > 1)
-    {
-        ft_memcpy(dst + dstlen, src, catlen - 1);
-        dst[srclen + dstlen] = '\0';
-    }
-    return (srclen + dstlen);
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	catlen = dstsize - dstlen;
+	if (catlen > 1)
+	{
+		ft_memcpy(dst + dstlen, src, catlen - 1);
+		dst[srclen + dstlen] = '\0';
+	}
+	return (srclen + dstlen);
 }
