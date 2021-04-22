0741d5e8f9c0ed802e82ad97d977a2321fe1b4c2
Kira <rarakira@gmail.com>
2021-04-22 14:33:21 +0300

    Update atoi. Add tests

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 16:16:51 by lbaela            #+#    #+#             */
-/*   Updated: 2021/04/21 09:32:48 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/04/22 14:32:43 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,45 +22,30 @@ static int	isspace(int ch)
 		return (0);
 }
 
-static int	issign(int ch, int *minus)
-{
-	if (ch == '-')
-	{
-		*minus = *minus + 1;
-		return (1);
-	}
-	else if (ch == '+')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int	ft_atoi(char *str)
+int	ft_atoi(const char *str)
 {
 	int	i;
-	int	minus;
+	int	sign;
 	int	result;
 
 	i = 0;
-	minus = 0;
 	result = 0;
 	while ((isspace(str[i])) && str[i] != '\0')
 	{
 		if (isspace(str[i]))
 			i++;
 	}
-	while ((str[i] == '-' || str[i] == '+') && str[i] != '\0')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (issign(str[i], &minus))
-			i++;
+		if (str[i] == '-')
+			sign = -1;
+		else if (str[i++] == '+')
+			sign = 1;
 	}
 	while ((str[i] >= '0' && str[i] <= '9') && str[i] != '\0')
 	{
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	if (minus % 2 == 1)
-		return (result * -1);
-	return (result);
+	return (result * sign);
 }
